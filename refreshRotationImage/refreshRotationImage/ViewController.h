bbd5ec45dfbb123c51eb8fced3991ab7271faaa3
tang <tangdream@163.com>
2016-03-31 19:56:01 +0800

    init apps

---
+++ refreshRotationImage/refreshRotationImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  refreshRotationImage
+//
+//  Created by tang on 16/3/31.
+//  Copyright © 2016年 shunzhitang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ refreshRotationImage/refreshRotationImage/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  refreshRotationImage
+//
+//  Created by tang on 16/3/31.
+//  Copyright © 2016年 shunzhitang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
