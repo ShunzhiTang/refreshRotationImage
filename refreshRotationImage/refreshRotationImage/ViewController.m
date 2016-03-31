9b8f745a86ff1f34b6701e380fe713f1c673b362
tang <tangdream@163.com>
2016-03-31 21:02:17 +0800

    刷新动画集成搞定

--- refreshRotationImage/refreshRotationImage/ViewController.m
+++ refreshRotationImage/refreshRotationImage/ViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "ViewController.h"
+#import "UIScrollView+UzysCircularProgressPullToRefresh.h"
 
-@interface ViewController ()
+#define IS_IOS7 (floor(NSFoundationVersionNumber) > NSFoundationVersionNumber_iOS_6_1 && floor(NSFoundationVersionNumber) <= NSFoundationVersionNumber_iOS_7_1)
+#define IS_IOS8  ([[[UIDevice currentDevice] systemVersion] compare:@"8" options:NSNumericSearch] != NSOrderedAscending)
+@interface ViewController () <UITableViewDataSource , UITableViewDelegate>
+
+@property (nonatomic,strong) UzysRadialProgressActivityIndicator *radialIndicator;
+@property (weak, nonatomic) IBOutlet UITableView *TSZTableView;
 
 @end
 
@@ -16,12 +22,87 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.title = @"test";
+    self.view.backgroundColor = [UIColor lightGrayColor];
+    
+//    self.TSZTableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+     self.TSZTableView.autoresizingMask = UIViewAutoresizingNone;
+    
+    self.TSZTableView.delegate  =self;
+    self.TSZTableView.dataSource = self;
+    
+    self.TSZTableView.showsVerticalScrollIndicator = NO;
+    self.TSZTableView.showsHorizontalScrollIndicator = NO;
+    self.TSZTableView.backgroundColor = [UIColor clearColor];
+    __weak typeof(self) weakSelf =self;
+    
+    [self.view addSubview:self.TSZTableView];
+    //Because of self.automaticallyAdjustsScrollViewInsets you must add code below in viewWillApper
+    
+    [_TSZTableView addPullToRefreshActionHandler:^{
+        [weakSelf insertRowAtTop];
+    }];
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+- (void)insertRowAtTop {
+    __weak typeof(self) weakSelf = self;
+    
+    int64_t delayInSeconds = 2.5;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+        [weakSelf.TSZTableView beginUpdates];
+//        [weakSelf.pData insertObject:[NSDate date] atIndex:0];
+//        [weakSelf.TSZTableView insertRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:0 inSection:0]] withRowAnimation:UITableViewRowAnimationBottom];
+        [weakSelf.TSZTableView endUpdates];
+        
+        //Stop PullToRefresh Activity Animation
+        [weakSelf.TSZTableView stopRefreshAnimation];
+    });
+}
+
+
+#pragma mark: uitableview的 代理方法
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    
+    return 20;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    static NSString *ID = @"cellID";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:ID];
+    
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:ID];
+    }
+    
+    
+    cell.detailTextLabel.text = @"detail";
+    
+    if (indexPath.row == 5) {
+         cell.textLabel.text = @"change image";
+        return cell;
+    }
+    cell.textLabel.text = @"nofound";
+    
+    return cell;
+    
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+     [self.TSZTableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+    if(indexPath.row == 5)
+    {
+        [self.TSZTableView.pullToRefreshView setImageIcon:[UIImage imageNamed:@"thunderbird"]];
+    }
 }
 
 @end
